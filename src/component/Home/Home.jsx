645b654c31579da8e050e91e416ea8c9260200af
Admin <nacer.abali.75@gmail.com>
2024-06-01 15:54:29 +0100

    service-updated

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,40 +1,47 @@
-import React , {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import "./Home.css";
 import MultiItemCarousel from './MultiItemCarousel';
 import RestaurantCard from '../Restaurant/RestaurantCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllRestaurantsAction } from '../State/Restaurant/Action';
-import { findCart } from '../State/Cart/Action';
 
 const Home = () => {
-  const dispatch= useDispatch()
-  const jwt = localStorage.getItem("jwt")
-  const  { restaurant,auth  }  =useSelector(store=>store)
-  console.log("restaurant" , restaurant)
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { restaurant ,auth } = useSelector(store => store);
+  console.log("restaurant", restaurant);
 
   useEffect(() => {
-    dispatch(getAllRestaurantsAction(jwt))
-}, [dispatch, jwt]);
+    dispatch(getAllRestaurantsAction(jwt));
+  }, [dispatch, jwt ]);
 
   return (
     <div className='pb-10'>
-      <section className='banner z-50 relative flex flex-col justify-center items-center'>
-        <div className='w-1/2 z-10 text-center'>
-          <p className='text-2xl lg:text-6xl   font-style: italic font-semibold py-5'>Qossos</p>
-          <p className='text-gray-200 text-5xl lg:text-4xl'> ! فيك جوع ؟ عندنا الحل</p>
+      <section className='banner z-50 relative flex justify-start items-center'>
+        <div className='text-content mt-5 w-1/2 z-10 text-left '>
+          <p className='text-2xl  text-yellow-500 lg:text-6xl italic font-semibold py-5'>Qossos</p>
+          <p className='text-gray-200 font-serif italic pl-10 text-5xl lg:text-4xl'> Bienvenue chez Qossos,
+         votre destination pour une cuisine délicieuse et authentique
+          </p>
+          {/* <p className='text-gray-200 text-5xl pl-60 lg:text-4xl'>! فيك جوع ؟ عندنا الحل</p> */}
+
+          <div className='flex justify-center mt-9'>
+          <button className="bg-yellow-500 hover:bg-yellow-700 pl-4 text-white font-bold py-2 px-6 rounded">Commander</button>
         </div>
-        <div className='cover absolute top-0 left right-0'></div>
-        <div className='fadout'></div>
+        </div>
+       
+        <div className='cover absolute top-0 left-0 right-0 bottom-0'></div>
       </section>
 
-      <section className='p-10 lg:py-20 lg:px-20 pt-10'>
-        <p className='text-2xl font-semibold text-gray-400 py-3 pb-5'> Top Meals </p>
-        <MultiItemCarousel/>
+      <section className='p-10 bg-yellow-500 lg:py-20 lg:px-20 pt-10'>
+        <p className='text-2xl mt-0 font-semibold text-black-400 py-3 pb-5'>Top Meals</p>
+        <MultiItemCarousel />
       </section>
 
       <section>
-        {auth.user &&(
-        <h1 className='text-2xl font-semibold text-gray-400 py-3'>Tleb a7ssan makayn 3end 9ossos</h1>)}
+        {auth.user && (
+          <h1 className='text-2xl font-semibold text-gray-400 px-20 py-5'>Tleb a7ssan makayn 3end 9ossos</h1>
+        )}
         <div className='flex flex-wrap items-center justify-around gap-5'>
             {
               auth.user && restaurant.restaurants.map((item)=><RestaurantCard item={item}/>)
--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -1,65 +1,60 @@
-import { Avatar, IconButton ,Badge, Box} from '@mui/material';
+import { Avatar, IconButton, Badge, Box } from '@mui/material';
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import "./Navbar.css";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './Navbar.css';
 import { Person } from '@mui/icons-material';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export const Navbar = () => {
-  const {auth,cart}=useSelector((store) => store);
-  const navigate=useNavigate();
-  const handleAvatarClick=()=>{
-    if(auth.user.role==="ROLE_CUSTOMER"){
-      navigate("/my-profile")
-    }
-    else{
-      navigate("/admin/restaurants")
-    }
+  const { auth, cart } = useSelector((store) => store);
+  const navigate = useNavigate();
 
-  }
+  const handleAvatarClick = () => {
+    if (auth.user.role === 'ROLE_CUSTOMER') {
+      navigate('/my-profile');
+    } else {
+      navigate('/admin/restaurants');
+    }
+  };
 
   return (
-    
-    <Box
-    
-    className='px-5 sticky z-50 py-[.8rem] bg-[#7ca41e] lg:px-8 flex justify-between'>
-  <div className='lg:mr-10 cursor-pointer flex items-center space-x-1'>
-    <img onClick={()=>navigate("/")} src="/9ossos.ico" alt="9ossos Logo" className="logo-image" />
-    <li onClick={()=>navigate("/")} className='logo font-style: italic font-semibold text-gray-100 text-2xl'>
-      Qossos
+    <Box className="navbar px-5 sticky z-50 py-[.8rem]">
+     <div className='lg:mr-10 cursor-pointer flex items-center space-x-1'>
+    <Avatar onClick={()=>navigate("/")} src="/9ossos.ico" alt="9ossos Logo" className="logo-image"></Avatar>  
+    <li onClick={()=>navigate("/")} className='logo   font: italic 1.2rem "Fira Sans", serif text-gray-100 text-2xl'>
+      Food service
     </li>
   </div>
-      <div className='flex items-center space-x-2 lg:space-x-10'>
-        <div>
-          <IconButton>
-            <SearchIcon sx={{ fontSize: "1.5rem" }} />
+      <div className="menu">
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Book Table</a>
+      </div>
+      <div className="actions">
+        <IconButton>
+          <SearchIcon sx={{ fontSize: '1.5rem' }} />
+        </IconButton>
+        <IconButton onClick={() => navigate('/cart')}>
+          <Badge color="primary" badgeContent={cart.cart?.item.length}>
+            <ShoppingCartIcon sx={{ fontSize: '1.5rem' }} />
+          </Badge>
+        </IconButton>
+        {auth.user ? (
+          <Avatar onClick={handleAvatarClick} sx={{ bgcolor: 'white', color: 'green' }}>
+            {auth.user.fullName ? auth.user.fullName[0].toUpperCase() : ''}
+          </Avatar>
+        ) : (
+          <IconButton onClick={() => navigate('/account/login')}>
+            <Person />
           </IconButton>
-        </div>
-        <div className=''>
-           {auth.user ? (
-  <Avatar onClick={handleAvatarClick} sx={{bgcolor:"white",color:"green"}}>
-    {auth.user.fullName ? auth.user.fullName[0].toUpperCase() : ""}
-  </Avatar>
-) : (
-  <IconButton onClick={()=>navigate("/account/login")}>
-    <Person/>
-  </IconButton>
-)}
-
-
-        </div>
-        <div>
-          <IconButton onClick={()=>navigate("/cart")}>
-            <Badge color="primary" badgeContent={cart.cart?.item.length
-}>
-            <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
-            </Badge>
-            
-          </IconButton>
-        </div>
+        )}
+        <a href="#" className="order-btn">Order Online</a>
       </div>
     </Box>
   );
-}
+};
+
+export default Navbar;
